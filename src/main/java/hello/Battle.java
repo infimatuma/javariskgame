8cf7df6ad0008306b926b8cedf792e0703a5bc89
infimatuma <infimatuma@gmail.com>
2019-12-22 21:40:47 +0200

    no message

--- src/main/java/hello/Battle.java
+++ src/main/java/hello/Battle.java
@@ -1,13 +1,18 @@
 package hello;
 
+import static java.lang.Integer.parseInt;
+
 public class Battle {
-    public resolve (GameArea attackingArea, int numberOfAttackers, GameArea defendingArea) {
-        attackingArea.setStr(String.valueOf(parseInt(attackingArea.getStr) - numberOfAttackers));
-        int numberOfDefenders = parseInt(defendingArea.getStr);
-        System.out.println(int a = int(Math.random() * ++)+1);
+    public void resolve (GameArea attackingArea, int numberOfAttackers, GameArea defendingArea) {
+        attackingArea.setStr(String.valueOf(parseInt(attackingArea.getStr()) - numberOfAttackers));
+        int numberOfDefenders = parseInt(defendingArea.getStr());
+
 
 // Defender won:
-defendingArea.setStr(String.valueOf(numberOfDefenders));
+        defendingArea.setStr(String.valueOf(numberOfDefenders));
 
+    }
+     public int rollDice() {
+         return (int) (Math.random() * 6)+1;
     }
 }
