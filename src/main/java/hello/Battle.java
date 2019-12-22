ad64eea82f0528a67d63d25b24001c073a3c977b
infimatuma <infimatuma@gmail.com>
2019-12-22 21:40:05 +0200

    no message

---
+++ src/main/java/hello/Battle.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Battle {
+    public resolve (GameArea attackingArea, int numberOfAttackers, GameArea defendingArea) {
+        attackingArea.setStr(String.valueOf(parseInt(attackingArea.getStr) - numberOfAttackers));
+        int numberOfDefenders = parseInt(defendingArea.getStr);
+        System.out.println(int a = int(Math.random() * ++)+1);
+
+// Defender won:
+defendingArea.setStr(String.valueOf(numberOfDefenders));
+
+    }
+}
